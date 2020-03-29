801cb3a1aeecb8c6954a2518f16edf27aa5768a0
Muhammad Hassaan <31187421+hassaanhameed786@users.noreply.github.com>
2020-03-29 19:24:23 +0500

    Add files via upload

---
+++ Javascript/object.js
@@ -0,0 +1,17 @@
+// objects  in JS 
+
+student = {
+    id: 176157,
+    name:'jon',
+    depart: 'CS',
+    School : 'FAST',
+    address ={
+        city: 'RYK',
+        District : 'RYK',
+        province  : 'Punjab',
+        country : 'Pak'
+    }
+
+};
+
+console.log(student.address.District);
