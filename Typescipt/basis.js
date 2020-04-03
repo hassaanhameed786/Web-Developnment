98ec2a5e389dd9c58cf2c3bfbcbfd0af8622ffcf
Muhammad Hassaan <31187421+hassaanhameed786@users.noreply.github.com>
2020-04-03 16:13:33 +0500

    Add files via upload

---
+++ Typescipt/basis.js
@@ -0,0 +1,29 @@
+"use strict";
+// Typescript => compiles into javascript first =>  Browser 
+var myString;
+//console.log('Hello world')
+var myNum;
+var myBool;
+myNum = 33;
+myString = "Learning Typescript";
+myBool = false;
+console.log(myNum);
+console.log(myString);
+console.log(myBool);
+// Arrays 
+var nummary;
+nummary = [1, 2, 3, 4, 5, 6,];
+console.log(nummary);
+//Array  2nd options 
+var starry;
+var boolarray;
+var numerary1;
+//const message: string = 'hello world';
+console.log();
+function add(a, b) {
+    return a + b;
+}
+//calling a fun 
+add(1, 2);
+add(1, 4);
+console.log("hey i m here ");
---
+++ Typescipt/basis.ts
@@ -0,0 +1,45 @@
+// Typescript => compiles into javascript first =>  Browser 
+let myString :string
+//console.log('Hello world')
+let  myNum:number
+let myBool: boolean
+
+
+myNum =33
+myString = "Learning Typescript"
+myBool =false
+
+console.log(myNum)
+console.log(myString)
+console.log(myBool)
+
+
+// Arrays 
+let nummary :number[];
+
+nummary =[1,2,3,4,5,6,];
+console.log(nummary)
+
+//Array  2nd options 
+
+let starry  : Array<string>;
+let boolarray : Array<boolean>;
+let numerary1 : Array<number>;
+
+
+
+
+
+//const message: string = 'hello world';
+console.log();
+
+function add(a: number, b:number) {
+    return a + b
+}
+
+//calling a fun 
+
+add(1, 2)
+add(1, 4)
+
+console.log("hey i m here ")
