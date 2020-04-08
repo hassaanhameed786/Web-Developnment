4288da46aac28eb10e6bd59e437c3396d2a4e2cf
Muhammad Hassaan <31187421+hassaanhameed786@users.noreply.github.com>
2020-04-08 21:03:11 +0500

    Add files via upload

---
+++ Typescipt/google-maps-JS/js/index.js
@@ -0,0 +1,17 @@
+///console.log("hello libs");
+
+
+function initMap() {
+    var sydney = { lat: -33.863276, lng: 151.107977 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: sydney,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+
+    });
+
+
+
+
+
+}
---
+++ Typescipt/google-maps-JS/todo.md
@@ -0,0 +1,17 @@
+# Plan Of Action
+
+
+- create wireframe 
+
+-set up html file 
+
+-import google maps into the window
+
+-add the text store container
+ 
+
+-add input box 
+
+- then add store list container 
+
+- add individuals 
